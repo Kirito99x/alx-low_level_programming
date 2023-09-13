e8615933f2405a5f2ed023204e0cbcb9ae8ddcda
Kirito99x <msaifeldin46@gmail.com>
2023-09-13 09:44:51 -0700

    0x02 task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,58 +1,40 @@
-#include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
- * add_strings - Adds two strings representing numbers
- * @num1: The first number
- * @num2: The second number
- * @result: The result of the addition
- */
-void add_strings(char num1[], char num2[], char result[])
-{
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
-	int carry = 0;
-	int i, j;
-
-	int max_len = (len1 > len2) ? len1 : len2;
-
-	memset(result, 0, 5000);
-
-	for (i = len1 - 1, j = len2 - 1; 
-	     i >= 0 || j >= 0 || carry; 
-	     i--, j--, max_len--)
-	{
-		int n1 = (i >= 0) ? num1[i] - '0' : 0;
-		int n2 = (j >= 0) ? num2[j] - '0' : 0;
-
-		int tmp = n1 + n2 + carry;
-		carry = tmp / 10;
-		result[max_len] = (tmp % 10) + '0';
-	}
-}
-
-/**
- * main - Program entry point
- * Return: 0 on success
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	char a[5000] = "1";
-	char b[5000] = "2";
-	char result[5000];
-	int i;
+	unsigned long int a, b, c, b1, b2, c1, c2;
 
-	printf("%s, %s", a, b);
+	b = 1;
+	c = 2;
 
-	for (i = 2; i < 98; i++)
+	printf("%lu", b);
+
+	for (a = 1; a < 91; a++)
 	{
-		add_strings(a, b, result);
-		printf(", %s", result);
-		strcpy(a, b);
-		strcpy(b, result);
+		printf(", %lu", c);
+		c = c + b;
+		b = c - b;
 	}
+	b1 = b / 1000000000;
+	b2 = b % 1000000000;
+	c1 = c / 1000000000;
+	c2 = c % 1000000000;
 
+	for (a = 92; a < 99; ++a)
+	{
+		printf(", %lu", c1 + (c2 / 1000000000));
+		printf("%lu", c2 % 1000000000);
+		c1 = c1 + b1;
+		b1 = c1 - b1;
+		c2 = c2 + b2;
+		b2 = c2 - b2;
+	}
 	printf("\n");
 	return (0);
 }
