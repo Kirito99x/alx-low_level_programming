b27c47c55bb06038dcb8709a8252b60fa7fea796
Kirito99x <msaifeldin46@gmail.com>
2023-09-18 14:14:05 -0700

    0x05 task file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - take a pointer to an in as parameter
+ *
+ * @n: in parameter
+ *
+ * Return: nothing 
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
