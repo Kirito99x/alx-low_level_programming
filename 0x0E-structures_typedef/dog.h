1c324fde5ae14ce2efcc91aa7bef004f16455e5d
Kirito99x <msaifeldin46@gmail.com>
2023-10-10 17:04:49 -0700

    0x0E task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,30 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * struct dog - a dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: owner's name
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
  *
- * Description: just a lone dog struct in a big kitty world
-*/
-typedef struct dog
+ * Description: struct dog
+ */
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
-#endif 
+#endif
