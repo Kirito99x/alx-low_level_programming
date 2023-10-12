3748b8cca5440cf089b52cb51b610e9aef40c43b
Kirito99x <msaifeldin46@gmail.com>
2023-10-12 13:00:12 -0700

    0x10task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,50 +1,24 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - Prints numbers, separated by a separator string.
+ * print_numbers - prints numbers, followed by a new line
  *
- * @separator: The string to be printed between numbers.
- * @n: The number of integers passed to the function.
- * ...: The variable number of integers to be printed.
+ * @separator: string to be printed between numbers
+ * @n: number of arguments
+ * Return: integer result or 0 if n == 0
  */
-void print_numbers(const char *separator, const unsigned int n, ...) {
-  /**
-   * Declare a va_list variable to store the variable number of arguments.
-   */
-  va_list args;
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = n;
 
-  /**
-   * Initialize the va_list variable to point to the first variable argument.
-   */
-  va_start(args, n);
-
-  /**
-   * Check if the separator argument is NULL. If it is not, print the separator
-   * to the console.
-   */
-  if (separator != NULL) {
-    printf("%s", separator);
-  }
-
-  /**
-   * Iterate over the remaining arguments and print them to the console, separated
-   * by the separator string (if provided).
-   */
-  for (unsigned int i = 0; i < n; i++) {
-    printf("%d", va_arg(args, int));
-    if (i < n - 1) {
-      printf("%s", separator);
-    }
-  }
-
-  /**
-   * Print a new line character to the console.
-   */
-  printf("\n");
-
-  /**
-   * Clean up the va_list variable.
-   */
-  va_end(args);
+	va_start(args, n);
+	for (; i; i--)
+	{
+		printf("%d", va_arg(args, int));
+		if (i - 1 != 0 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
 }
-
