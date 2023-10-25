8dae816b7403b9a52d330f6f8602b85d851b352b
Kirito99x <msaifeldin46@gmail.com>
2023-10-25 05:32:11 -0700

    0x13

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,22 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * listint_len - returns the number of elements in a list
- * @h: pinter to head
- * Return: len
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: listint_t list
+ *
+ * Return: number of elements in h
 */
 
 size_t listint_len(const listint_t *h)
 {
-	size_t len = 0;
+	size_t num_elmnt = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		num_elmnt++;
 		h = h->next;
-		++len;
-		}
-		return (len);
+	}
+
+	return (num_elmnt);
 }
